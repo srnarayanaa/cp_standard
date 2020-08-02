fab27de26049c0f24acb3011f72c09d948c234bf
Narayanaa <srnarayanaa@gmail.com>
2020-08-02 08:56:12 +0530

    Update sieve of Eratosthenes.py

--- sieve of Eratosthenes.py
+++ sieve of Eratosthenes.py
@@ -5,8 +5,10 @@ def sieve(N):
     primes=[True]*N 
     primes[0]=primes[1]=False
     for (i, isprime) in enumerate(primes):
+        #print(i,primes)
         if isprime:
             yield i 
+            #instead of 2*i, i*i optimization -> this for loop makes all factors false
             for j in range(i*i, N, i):     
                 primes[j] = False
 
